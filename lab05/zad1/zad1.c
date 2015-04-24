32766b4b70c3fe51ee14cc833f002eaca603090d
marg <mksalawa@gmail.com>
2015-04-24 08:07:41 +0200

    lab05 - zad1, little fixes

--- lab05/zad1/zad1.c
+++ lab05/zad1/zad1.c
@@ -47,6 +47,15 @@ void spawnLs(int lsToGrep[], int grepToWc[]) {
     }
 }
 
+void execPipedLs(int lsToGrep[]) {
+    close(lsToGrep[READ_END]);
+
+    dup2(lsToGrep[WRITE_END], STDOUT_FILENO);
+    close(lsToGrep[WRITE_END]);
+
+    execlp("ls", "ls", "-l", NULL);
+}
+
 void spawnGrep(int lsToGrep[], int grepToWc[]) {
     pid_t pid = fork();
     if (pid == 0) {
@@ -54,32 +63,31 @@ void spawnGrep(int lsToGrep[], int grepToWc[]) {
     }
 }
 
+void execPipedGrep(int lsToGrep[], int grepToWc[]) {
+    close(lsToGrep[WRITE_END]);
+
+    dup2(lsToGrep[READ_END], STDIN_FILENO);
+    close(lsToGrep[READ_END]);
+
+    close(grepToWc[READ_END]);
+
+    dup2(grepToWc[WRITE_END], STDOUT_FILENO);
+    close(grepToWc[WRITE_END]);
+
+    execlp("grep", "grep", "^d", NULL);
+}
+
 void performWc(int lsToGrep[], int grepToWc[]) {
     closeUnusedPipe(lsToGrep);
     execPipedWc(grepToWc);
 }
 
-void execPipedLs(int lsToGrep[]) {
-    close(lsToGrep[READ_END]);
-    dup2(lsToGrep[WRITE_END], STDOUT_FILENO);
-    close(lsToGrep[WRITE_END]);
-    execlp("ls", "ls", "-l", NULL);
-}
-
-void execPipedGrep(int lsToGrep[], int grepToWc[]) {
-    close(lsToGrep[WRITE_END]);
-    dup2(lsToGrep[READ_END], STDIN_FILENO);
-    close(lsToGrep[READ_END]);
-    close(grepToWc[READ_END]);
-    dup2(grepToWc[WRITE_END], STDOUT_FILENO);
-    close(grepToWc[WRITE_END]);
-    execlp("grep", "grep", "^g", NULL);
-}
-
 void execPipedWc(int grepToWc[]) {
     close(grepToWc[WRITE_END]);
+
     dup2(grepToWc[READ_END], STDIN_FILENO);
     close(grepToWc[READ_END]);
+
     execlp("wc", "wc", "-l", NULL);
 }
 
